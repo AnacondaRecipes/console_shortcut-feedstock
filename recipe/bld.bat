06b8b7dc07086a9c5b64a7740df5227194135bfc
Marco Esters <mesters@anaconda.com>
2024-06-21 11:54:06 -0700

    Replace icon

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
 set MENU_DIR=%PREFIX%\Menu
 IF NOT EXIST (%MENU_DIR%) mkdir %MENU_DIR%
 
-copy %RECIPE_DIR%\Iconleak-Atrous-Console.ico %MENU_DIR%
+copy %RECIPE_DIR%\anaconda_console.ico %MENU_DIR%
 if errorlevel 1 exit 1
 
 copy %RECIPE_DIR%\menu-windows.json %MENU_DIR%\console_shortcut.json
